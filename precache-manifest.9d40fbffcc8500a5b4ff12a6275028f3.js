716cf3f211058c23e6d0c30af92c930dc280e2d1
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2020-10-24 22:46:31 +0100

    Updates

---
+++ precache-manifest.9d40fbffcc8500a5b4ff12a6275028f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30bceabcc9ac2d69a057c902476ea9c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4bbba39650ca41360361",
+    "url": "/monsters-rolodex/static/css/main.2cb70fdd.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bbba39650ca41360361",
+    "url": "/monsters-rolodex/static/js/main.73ca19ea.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
